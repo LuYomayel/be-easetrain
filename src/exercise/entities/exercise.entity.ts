4319702587668b506430a2fe3b2f99b8d5b2504f
Luciano Yomayel <l.yomayel@gmail.com>
2023-10-29 20:30:57 +1100

    change on exercise entity

--- src/exercise/entities/exercise.entity.ts
+++ src/exercise/entities/exercise.entity.ts
@@ -9,11 +9,50 @@ export class Exercise {
   @Column()
   name: string;
 
-  @Column()
+  @Column({ nullable: true })
+  type: string;
+
+  @Column({ nullable: true })
   repetitions: number;
 
-  @Column()
+  @Column({ nullable: true })
   sets: number;
+
+  @Column({ nullable: true })
+  duration: number;
+
+  @Column({ nullable: true })
+  weight: number;
+
+  @Column({ nullable: true })
+  distance: number;
+
+  @Column({ nullable: true })
+  intensity: string;
+
+  @Column({ nullable: true })
+  equipment: string;
+
+  @Column({ nullable: true })
+  difficulty: string;
+
+  @Column({ nullable: true, type: 'text' })
+  instructions: string;
+
+  @Column({ nullable: true })
+  multimedia: string;
+
+  @Column({ nullable: true })
+  rest: number;
+
+  @Column({ nullable: true })
+  order: number;
+
+  @Column({ nullable: true })
+  unitOfMeasure: string;
+
+  @Column({ nullable: true, type: 'text' })
+  notes: string;
 
   @ManyToOne(() => Workout, (workout) => workout.exercises)
   workout: Workout;
