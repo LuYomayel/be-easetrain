8658176fbb1507a7c0b3807d3f74dfd27309ae6b
Luciano Yomayel <l.yomayel@gmail.com>
2024-02-10 18:54:12 +1100

    import

--- src/exercise/exercise.module.ts
+++ src/exercise/exercise.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ExerciseService } from './exercise.service';
 import { ExerciseController } from './exercise.controller';
+import { Exercise } from './entities/exercise.entity';
+import { BodyArea } from './entities/body-area.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([BodyArea, Exercise])],
   controllers: [ExerciseController],
   providers: [ExerciseService],
 })
