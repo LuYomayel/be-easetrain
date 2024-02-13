f28381dcd4a2396bd77f17e31ef2785cf51abc17
Luciano Yomayel <l.yomayel@gmail.com>
2024-02-13 19:02:23 +1000

    probando save plan

--- src/workout/workout.module.ts
+++ src/workout/workout.module.ts
@@ -3,8 +3,12 @@ import { WorkoutService } from './workout.service';
 import { WorkoutController } from './workout.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Workout } from './entities/workout.entity';
+import { ExerciseGroup } from '../exercise/entities/exercise-group.entity';
+import { ExerciseInstance } from '../exercise/entities/exercise.entity';
 @Module({
-  imports: [TypeOrmModule.forFeature([Workout])],
+  imports: [
+    TypeOrmModule.forFeature([Workout, ExerciseGroup, ExerciseInstance]),
+  ],
   controllers: [WorkoutController],
   providers: [WorkoutService],
 })
